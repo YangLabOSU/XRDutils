a7c38507a3f8af29e631698a2d0a25d17324abce
justinm678 <justinmichel678@gmail.com>
2022-08-11 11:45:27 -0400

    processing works now

--- XRDutils_getdata.py
+++ XRDutils_getdata.py
@@ -3,104 +3,149 @@ import numpy as np
 import os
 import sys
 import getopt
+import matplotlib.pyplot as plt
 
+class XRDmachine:
+    #Class defining XRD machine data header format. 
+    def __init__(self, name):
+        self.name = name
+        if name == 'ece':
+            self.timestp = 'Time: '
+            self.datestp = 'Date: '
+            self.stepsize = 'Scan Stepsize: '
+            self.sttwothetaangl = '          2Theta    '
+            self.datstart = 'Position    '
+            self.tpp = 'Counting time per point: '
+            self.wvlnth = 'Wavelength: '
+            self.delimiter= '    '
+        elif name == 'NTW':
+            self.timestp = 'Time='
+            self.datestp = 'Date='
+            self.stepsize = 'Increment='
+            self.sttwothetaangl = 'Start='
+            self.datstart = '     Angle'
+            self.tpp = 'Time='
+            self.wvlnth = 'ActuallyUsedLambda='
+            self.delimiter= ','
 
-def getdata(fname, sr=43):
-    path = ''
-    if sr == 43:
-        for line in open(path+fname):
-           if line.startswith('SampleID:'):
-              material = line.split(':')[1]
+class scdata:
+    #Class defninig scan data
+    def __init__(self, name, machine):
+        self.name = name
+        self.fname = name.split('\\')[-1]
+        self.fold = name.split(self.fname)[0]
+        self.machine = machine
+
+def getdata(fname, machinenm=''):
+    #function to get the metadata and scan data from the file and populate the scdata class to save the data
+    print('getting data from '+fname)
+
+    # get the machine type that the scan was done on. 
+    # If 'machinenm' option is not passed, determine this from the file extension
+    if machinenm != '':
+        machine = XRDmachine(machinenm)
     else:
-        material = fname
-    data = pd.read_csv(path+fname, skiprows=sr,
-                       delimiter='    ', engine='python')
-    return material, data
-
-
-def plotdata(fnames, sctypes, note='', legend=False, grid=False, zeroshift=0, shiftspeak=True, annotatepeaks=False, machine='ece'):
-
-    for i in range(len(fnames)):
-        fname = fnames[i]
-        sctype = sctypes[i]
-        if machine == 'ece':
-            mat, d = getdata(fname, sr=43)
-        elif machine == 'cemas':
-            mat, d = getdata(fname, sr=381)
-        title = fname.split('/')[1].split('/')[0]+mat+' '+note
-        # plot
-        if sctype == 'xrd':
-            plt.figure(1)
-            pkshift = 0
-            if shiftspeak:
-                indx_max = np.argmax(d.Count)
-                pkshift = d.Position[indx_max]*2
-            if annotatepeaks:
-                peaks, _ = find_peaks(d.Count, height=30, distance=200)
-                # print(peaks)
-                for peak in peaks:
-                    plt.annotate(str(2*d.Position[peak]+zeroshift-pkshift), [
-                                 2*d.Position[peak]+zeroshift-pkshift, d.Count[peak]+30], fontsize=7)
-
-            plt.title(title)
-            plt.semilogy(2*d.Position+zeroshift-pkshift,
-                         d.Count, lw=1, label=title)
-            plt.xlabel(r'$\Delta 2\theta$(deg)')
-            if legend:
-                plt.legend()
-            if grid:
-                plt.grid()
-            plt.ylabel('intensity(cps)')
-        elif sctype == 'xrr':
-            plt.figure(2)
-            plt.title(title)
-            plt.semilogy(2*d.Position, d.Count, lw=1, label=title)
-            plt.xlabel(r'$\Delta 2\theta$(deg)')
-            if legend:
-                plt.legend()
-            if grid:
-                plt.grid()
-            plt.ylabel('intensity(cps)')
-        elif sctype == 'rc':
-            plt.figure(3)
-            plt.title(title)
-            plt.plot(d.Position, d.Count, lw=1, label=title)
-            plt.xlabel(r'$\Delta \Omega$(deg)')
-            if legend:
-                plt.legend()
-            if grid:
-                plt.grid()
-            plt.ylabel('intensity(cps)')
-        else:
-            print('valid types are xrd, xrr, or rc')
-
-    plt.show()
-
-
-def plotinfolder(fpath, **kwargs):
-    fnames1 = [fpath+fname for fname in os.listdir(fpath)]
-    fnames = []
-    for fname in fnames1:
         if fname[-3:] == 'X01':
-            fnames.append(fname)
-    sctypes = []
-    for fname in fnames:
-        if 'XRD_' in fname:
-            sctypes.append('xrd')
-        elif 'XRR_' in fname:
-            sctypes.append('xrr')
-        elif 'RC_' in fname:
-            sctypes.append('rc')
+            machine = XRDmachine('ece')
+        elif fname[-3:] == 'txt':
+            machine = XRDmachine('NTW')
+    
+    #define the scan data class and populate it using the machine format settings.
+    d=scdata(fname, machine)
+    with open(d.name) as file:
+        for num, line in enumerate(file, 1):
+            if machine.timestp in line:
+                d.timestp = line.split(machine.timestp)[1].split(' ')[0]
+            if machine.datestp in line:
+                d.datestp = line.split(machine.datestp)[1].split(' ')[0]
+            if machine.stepsize in line:
+                d.stepsize = float(line.split(machine.stepsize)[1].split(' ')[0])
+            if machine.sttwothetaangl in line:
+                d.sttwothetaangl = float(line.split(machine.sttwothetaangl)[1].split(' ')[0])
+            if machine.datstart in line:
+                d.datstart = num-1
+            if machine.tpp in line:
+                try:
+                    d.tpp = float(line.split(machine.tpp)[1].split(' ')[0])
+                except:
+                    """
+                    There are 2 Time= lines for NTW data, so if the result can't be converted, skip it.
+                    """
+            if machine.wvlnth in line:
+                d.wvlnth = float(line.split(machine.wvlnth)[1].split(' ')[0])
+
+    #get the scan data using the header information gathered
+    d.datadf = pd.read_csv(d.name, skiprows=d.datstart,
+                       delimiter=d.machine.delimiter, engine='python')
+
+    return d
+
+def savedata(d):
+    #save data object to '.xrd'(text) file at the location it was opened from
+    fname=d.fold+d.fname.split('.')[0]+'proc.xrd'
+    with open(fname, 'w') as f:
+        #save header info
+        f.write("filename: "+d.fname+'\n')
+        f.write("process folder: "+d.fold+'\n')
+        f.write("machine: "+d.machine.name+'\n')
+        f.write("date: "+d.datestp+'\n')
+        f.write("time: "+d.timestp+'\n')
+        f.write("start 2theta angle: "+str(d.sttwothetaangl)+'\n')
+        f.write("step size: "+str(d.stepsize)+'\n')
+        f.write("time per point: "+str(d.tpp)+'\n')
+        f.write("wavelength: "+str(d.wvlnth)+'\n')
+        f.write("x, y\n")
+
+        #save data
+        #if the machine is ece, shift x to be 2theta (default is theta) and add offset (default is relative)
+        for i in range(len(d.datadf.iloc[:, 0])):
+            if d.machine.name == 'ece':
+                f.write(str(d.sttwothetaangl-2*d.datadf.iloc[0, 0]+2*d.datadf.iloc[i, 0]))
+            else:
+                f.write(str(d.datadf.iloc[i, 0]))
+            f.write(', ')
+            f.write(str(d.datadf.iloc[i, 1])+'\n')
+
+def getdatainfolder(fpath, repl=False, sdsearch=True):
+    #only look for files with these extensions
+    extf=['.txt','.X01']
+    flist1=[]
+    #get file names, depending on if subdirectories are searched or not
+    if sdsearch:
+        for path, subdirs, files in os.walk(fpath):
+            for name in files:
+                if any(ext in name for ext in extf):
+                    flist1.append(os.path.join(path, name))
+    else:
+        for name in os.listdir(fpath):
+            if any(ext in name for ext in extf):
+                flist1.append(os.path.join(path, name))
+    
+    #check if processed file is already there - replace it if 'repl' is True
+    flist=[]
+    for f in flist1:
+        if os.path.isfile(f[:-4]+'proc.xrd'):
+            if repl:
+                print('xrd file {} already exists, replacing...'.format(f))
+                flist.append(f)
+            else:
+                print('xrd file {} already exists, NOT replacing...'.format(f))
         else:
-            sctypes.append('xrd')
-    plotdata(fnames, sctypes, **kwargs)
+            flist.append(f)
+
+    for fname in flist:
+        d=getdata(fname)
+        savedata(d)
 
 
 def main(argv):
-    helpmsg = 'XRDutils_getdata -i <inputfolder>'
-    inputfolder = ''
+    helpmsg = 'XRDutils_getdata.py -i <inputfolder/file> \n-r for replace already processed\n-s for ignore subdirs'
+    repl = False
+    sdsearch = True
+    #default folder to look for new XRD data in. (will also look in +1 level subfolders)
+    inputfolder = r"C:\Users\justi\OneDrive\Documents\Material_Characterization\XRD"
     try:
-        opts, args = getopt.getopt(argv,"hi:",["ifolder="])
+        opts, args = getopt.getopt(argv,"hrsi:",["ifolder="])
     except getopt.GetoptError:
         print(helpmsg)
         sys.exit(2)
@@ -108,9 +153,20 @@ def main(argv):
         if opt == '-h':
             print(helpmsg)
             sys.exit()
+        elif opt == '-r':
+            repl = True
+            print('Replacing previously processed data files...')
+        elif opt == '-s':
+            sdsearch = False
+            print('Ignoring subdirectories...')
         elif opt in ("-i", "--ifile"):
             inputfolder = arg
-            print('Input folder is '+ inputfolder)
+            print('Input file/folder is '+ inputfolder)
+    if '.' in inputfolder:
+        d=getdata(inputfolder)
+        savedata(d)
+    else:
+        getdatainfolder(inputfolder, sdsearch=sdsearch, repl=repl)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
